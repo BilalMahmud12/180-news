5a7c2371d758354d45823da883dbf29ad3f936f1
Bilal Mahmud <jzrgrp@gmail.com>
2021-03-22 04:12:35 +0300

    single post updated

--- plugins/midorsoft/publish/routes.php
+++ plugins/midorsoft/publish/routes.php
@@ -11,3 +11,18 @@ Route::get('/go/{link}', function (){
     $construct = '/' . $url->slug . '/' .date( "d-m-Y", strtotime( $url->created_at ) );
     return redirect($construct);
 });
+
+
+Route::get('date', function (){
+    $date = new Date();
+    $fmt = datefmt_create(
+        'ars',
+        IntlDateFormatter::LONG ,
+        IntlDateFormatter::SHORT,
+        'Asia/Istanbul',
+        IntlDateFormatter::GREGORIAN,
+
+    );
+    return datefmt_format($fmt, $date);
+    //return strftime("%e %B %Y %A", time());
+});
